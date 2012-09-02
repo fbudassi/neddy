2d4b4b4d9263af1b74091bd3a2f049f3840527e6
Federico Budassi <federico.budassi@gmail.com>
2012-09-02 18:15:05 -0300

    added constants from the config file

--- neddybenchmark/src/main/java/com/fbudassi/neddy/benchmark/benchmarks/RestBenchmark.java
+++ neddybenchmark/src/main/java/com/fbudassi/neddy/benchmark/benchmarks/RestBenchmark.java
@@ -1,5 +1,6 @@
 package com.fbudassi.neddy.benchmark.benchmarks;
 
+import com.fbudassi.neddy.benchmark.config.Config;
 import com.fbudassi.neddy.benchmark.pipeline.StaticContentPipelineFactory;
 import org.jboss.netty.channel.ChannelPipelineFactory;
 import org.slf4j.Logger;
@@ -12,6 +13,12 @@ import org.slf4j.LoggerFactory;
 public class RestBenchmark implements Benchmark {
 
     private static final Logger logger = LoggerFactory.getLogger(RestBenchmark.class);
+    // Configuration constants.
+    private static final int NUMSPEAKERS = Config.getIntValue(Config.KEY_NUMSPEAKERS);
+    private static final int NUMCATEGORIES = Config.getIntValue(Config.KEY_SPEAKER_NUMCATEGORIES);
+    private static final int DELAYBETWEENMESSAGES = Config.getIntValue(Config.KEY_SPEAKER_DELAYBETWEENMESSAGES);
+    private static final String RESOURCE_CATEGORY = Config.getValue(Config.KEY_RESOURCE_CATEGORY);
+    private static final String RESOURCE_LISTENERS = Config.getValue(Config.KEY_RESOURCE_LISTENERS);
 
     /**
      * Rest benchmark.
