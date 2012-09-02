331d590d9e5fb27a6bb88cacb5f8e1170171ce0f
Federico Budassi <federico.budassi@gmail.com>
2012-09-02 18:16:04 -0300

    code formatted. functions that are necessary defined. Code adapted from the example of Netty.

--- neddybenchmark/src/main/java/com/fbudassi/neddy/benchmark/handler/WebSocketClientHandler.java
+++ neddybenchmark/src/main/java/com/fbudassi/neddy/benchmark/handler/WebSocketClientHandler.java
@@ -8,56 +8,86 @@ import org.jboss.netty.channel.MessageEvent;
 import org.jboss.netty.channel.SimpleChannelUpstreamHandler;
 import org.jboss.netty.handler.codec.http.HttpResponse;
 import org.jboss.netty.handler.codec.http.websocketx.CloseWebSocketFrame;
-import org.jboss.netty.handler.codec.http.websocketx.PongWebSocketFrame;
 import org.jboss.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import org.jboss.netty.handler.codec.http.websocketx.WebSocketClientHandshaker;
 import org.jboss.netty.handler.codec.http.websocketx.WebSocketFrame;
 import org.jboss.netty.util.CharsetUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+/**
+ * 
+ * @author fbudassi
+ */
 public class WebSocketClientHandler extends SimpleChannelUpstreamHandler {
 
+    private static final Logger logger = LoggerFactory.getLogger(StaticContentBenchmarkHandler.class);
     private final WebSocketClientHandshaker handshaker;
 
+    /**
+     * Handler constructor.
+     *
+     * @param handshaker
+     */
     public WebSocketClientHandler(WebSocketClientHandshaker handshaker) {
         this.handshaker = handshaker;
     }
 
+    /**
+     * Executed when channel is opened.
+     *
+     * @param ctx
+     * @param e
+     * @throws Exception
+     */
     @Override
-    public void channelClosed(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
-        System.out.println("WebSocket Client disconnected!");
+    public void channelOpen(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
     }
 
+    /**
+     * Executed when a new message is received.
+     *
+     * @param ctx
+     * @param e
+     * @throws Exception
+     */
     @Override
     public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) throws Exception {
         Channel ch = ctx.getChannel();
+
+        // Finish with the Websocket handshake if it's not done yet.
         if (!handshaker.isHandshakeComplete()) {
             handshaker.finishHandshake(ch, (HttpResponse) e.getMessage());
-            System.out.println("WebSocket Client connected!");
             return;
         }
 
+        // Check for bad messages.
         if (e.getMessage() instanceof HttpResponse) {
             HttpResponse response = (HttpResponse) e.getMessage();
             throw new Exception("Unexpected HttpResponse (status=" + response.getStatus() + ", content="
                     + response.getContent().toString(CharsetUtil.UTF_8) + ")");
         }
 
+        // Do the necessary stuff to process the websocket frame that was received.
         WebSocketFrame frame = (WebSocketFrame) e.getMessage();
         if (frame instanceof TextWebSocketFrame) {
             TextWebSocketFrame textFrame = (TextWebSocketFrame) frame;
-            System.out.println("WebSocket Client received message: " + textFrame.getText());
-        } else if (frame instanceof PongWebSocketFrame) {
-            System.out.println("WebSocket Client received pong");
+            //TODO: process frame
         } else if (frame instanceof CloseWebSocketFrame) {
-            System.out.println("WebSocket Client received closing");
             ch.close();
         }
     }
 
+    /**
+     * Executed when an exception is caught.
+     *
+     * @param ctx
+     * @param e
+     * @throws Exception
+     */
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) throws Exception {
-        final Throwable t = e.getCause();
-        t.printStackTrace();
+        logger.error("Error in handler", e.getCause());
         e.getChannel().close();
     }
 }
